876d381d17e5782a0847970dc2e5fd066b4638cf
VishCoder556 <133464274+VishCoder556@users.noreply.github.com>
2024-12-01 08:38:43 -0600

    Add files via upload

---
+++ test.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+CORS(app)
+
+def scrape_bing_images(query):
+    search_url = f"https://www.bing.com/images/search?q={query}"
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    }
+    response = requests.get(search_url, headers=headers)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
+        image_elements = soup.find_all("img")
+        image_urls = []
+        for img in image_elements:
+            img_url = img.get("src")
+            if img_url and img_url.startswith("http") and not img_url.startswith("https://r.bing.com"):
+                image_urls.append(img_url)
+        
+        return image_urls
+    else:
+        return []
+
+@app.route('/scrape-images', methods=['GET'])
+def scrape_images():
+    query = request.args.get('query', '')
+    image_urls = scrape_bing_images(query)
+    return jsonify(image_urls)
+
+if __name__ == '__main__':
+    app.run(debug=True)
